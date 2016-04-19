effd7485d58ef34410e7d9cab3269a52a97dce10
moskung111222 <moskung111222@gmail.com>
2016-04-19 16:49:25 +0700

    -checkpoint 1-
    สร้างหน้าต่างพื้นหลังให้ ออกมาโดยมีความกว้าง 400 x 600

---
+++ Main.java
@@ -0,0 +1,18 @@
+//package f2.spw;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Space WarII");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 720);//add size
+		
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.setVisible(true);
+		
+		
+	}
+}
