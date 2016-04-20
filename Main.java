21e80176f8268a1d492979d7d76eb9fd045f08a3
moskung111222 <moskung111222@gmail.com>
2016-04-20 20:45:16 +0700

    -checkpoint 2-
    สร้างยานอยู่ตรงกลางโดยไม่เคลื่อนไหวไม่ได้

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-//package f2.spw;
+package f2.spw;
 
 import java.awt.BorderLayout;
 
@@ -6,13 +6,18 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args){
-		JFrame frame = new JFrame("Space WarII");
+		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 720);//add size
-		
+		frame.setSize(400, 600);//add size
 		frame.getContentPane().setLayout(new BorderLayout());
+		
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		GamePanel gp = new GamePanel();
+		/*GameEngine engine = new GameEngine(gp, v);*/
+		/*frame.addKeyListener(engine);*/
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
-		
+		/*engine.start();*/
 	}
 }
