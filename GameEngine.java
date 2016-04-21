1d20beceaf7bc5e0919d46543391eef04aa6bdbc
moskung111222 <moskung111222@gmail.com>
2016-04-21 20:15:32 +0700

    -checkpoint3-
    สร้างให้เคลื่อนที่ซ้ายขวา สร้างคลาส enemy แต่ไม่ได้ให้หล่นลงมา สร้างสกอร์ให้เก็บค่าแต่ไม่ให้ปรากฎ

---
+++ GameEngine.java
@@ -0,0 +1,83 @@
+package f2.spw;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine implements KeyListener, GameReporter{
+	GamePanel gp;
+		
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private SpaceShip v;	
+	
+	private Timer timer;
+	
+	private long score = 0;
+	private double difficulty = 0.15; //add difficulty 
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		
+		timer = new Timer(50, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//process();
+			}
+		});
+		timer.setRepeats(true);
+		
+	}
+	
+	public void start(){
+		timer.start();
+	}
+	
+	
+	
+	
+	
+	void controlVehicle(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			v.move(-1);
+			break;
+		case KeyEvent.VK_RIGHT:
+			v.move(1);
+			break;
+		case KeyEvent.VK_D:
+			difficulty += 0.15;
+			break;
+		}
+	}
+
+	public long getScore(){
+		return score;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+}
